d90948500bd1dba13dbbd0625bab4014da214e56
saicharitha6 <saicharitha1932@gmail.com>
2024-01-30 13:38:54 +0530

    added components

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Router, Scene, Stack } from "react-native-router-flux";
+import Products from "./screens/Products";
+import ProductInfo from "./screens/ProductInfo";
+import SignupScreen from "./screens/SignUp";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Router>
+      <Stack key="root">
+        <Scene key="products" component={Products} hideNavBar />
+        <Scene key="ProductInfo" component={ProductInfo} hideNavBar />
+        <Scene key="SignupScreen" component={SignupScreen} hideNavBar/>
+      </Stack>
+    </Router>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ constants/url.js
@@ -0,0 +1,3 @@
+const baseURL = "https://1adc-223-196-171-134.ngrok-free.app";
+
+export default baseURL;
